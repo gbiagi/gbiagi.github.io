55a15c11eb4ea055e4d362c032488fac4eb3509c
Geanfranco Biagi <118919877+gbiagi@users.noreply.github.com>
2025-10-04 00:31:32 +0200

    fix

--- src/React/SkillsList.tsx
+++ src/React/SkillsList.tsx
@@ -37,9 +37,6 @@ const CategoryIcons = {
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
       className="w-6 h-6 text-[var(--sec)] opacity-70">
       <path stroke="none" d="M0 0h24v24H0z" fill="none" />
       <path d="M2 6m0 2a2 2 0 0 1 2 -2h16a2 2 0 0 1 2 2v8a2 2 0 0 1 -2 2h-16a2 2 0 0 1 -2 -2z" />
